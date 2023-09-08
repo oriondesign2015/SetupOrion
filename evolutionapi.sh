6df04f29db0daa0a485eb27bf28db8618f69b560
oriondesign2015 <139019565+oriondesign2015@users.noreply.github.com>
2023-09-08 19:52:09 -0300

    Update evolutionapi.sh

--- evolutionapi.sh
+++ evolutionapi.sh
@@ -160,7 +160,18 @@ sudo apt update -y
 
 sudo apt upgrade -y
 
-curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash - && apt-get install -y nodejs
+sudo apt-get update
+sudo apt-get install -y ca-certificates curl gnupg
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+
+NODE_MAJOR=16
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+
+sudo apt-get update
+sudo apt-get install nodejs -y
+
+#curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash - && apt-get install -y nodejs
 
 npm install -g npm@latest
 
