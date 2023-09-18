d756deec46f5c8ab3ff121765644f77e7d614cae
oriondesign2015 <139019565+oriondesign2015@users.noreply.github.com>
2023-09-18 19:10:56 -0300

    Update chatwoot.sh

--- chatwoot.sh
+++ chatwoot.sh
@@ -197,6 +197,11 @@ sudo apt update
 
 apt upgrade -y
 
+sudo apt-get update
+sudo apt-get install -y ca-certificates curl gnupg
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+
 sudo timedatectl set-timezone America/Sao_Paulo
 
 #
