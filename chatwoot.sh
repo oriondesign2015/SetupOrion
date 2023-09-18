46a22e201fabf3dab5af456c75dd50bc95416c0d
oriondesign2015 <139019565+oriondesign2015@users.noreply.github.com>
2023-09-18 19:56:20 -0300

    Update chatwoot.sh

--- chatwoot.sh
+++ chatwoot.sh
@@ -197,10 +197,10 @@ sudo apt update
 
 apt upgrade -y
 
-sudo apt-get update
-sudo apt-get install -y ca-certificates curl gnupg
-sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+#sudo apt-get update
+#sudo apt-get install -y ca-certificates curl gnupg
+#sudo mkdir -p /etc/apt/keyrings
+#curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
 
 sudo timedatectl set-timezone America/Sao_Paulo
 
@@ -256,7 +256,13 @@ systemctl daemon-reload && systemctl restart chatwoot.target
 # HABILITANDO CONFIGURAÇÕES OCULTAS DO CHATWOOT
 #
 
-sudo -u postgres psql -d chatwoot_production -c "UPDATE installation_configs SET locked = false;"
+
+sudo -u postgres psql -d chatwoot_production <<EOF
+update installation_configs set locked = false;
+\q
+EOF
+
+#sudo -u postgres psql -d chatwoot_production -c "UPDATE installation_configs SET locked = false;"
 
 #
 # FIM DE HABILITANDO CONFIGURAÇÕES OCULTAS DO CHATWOOT
